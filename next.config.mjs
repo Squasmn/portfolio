934c8e7d6178edfa02c1955ad24387c75293fa42
david emmerich <squasmn.withem@gmail.com>
2024-05-27 00:53:23 +0200

    add loader for images

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   distDir: "build", // Specify custom build directory
   images: {
     domains: ["via.placeholder.com", "cdn.jsdelivr.net"],
+    loader: "default",
   },
 };
 
