6cf7465c579085a85df236d4b93fa7244dffe0b0
david emmerich <squasmn.withem@gmail.com>
2024-05-27 12:48:56 +0200

    try workaround for images

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -65,7 +65,7 @@ export const FloatingNav = ({
       >
         <div className="icon">
           <Image
-            src="/images/Icon.jpg"
+            src="https://github.com/Squasmn/portfolio/blob/main/public/images/Icon.jpg"
             alt="icon image"
             width={64}
             height={64}
